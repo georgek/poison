dd7bd74add9f379cf4af21ee05dba9f1802e1ee7
George Kettleborough <george@gpkb.org>
2024-01-29 22:14:47 +0000

    Better contents scrollspy

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -6,33 +6,19 @@
     to get it working for my use case.  Thanks, Bramus!
 */
 
-let activeElement = null;
 window.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver(entries => {
-        if (entries) {
-            const contents = document.getElementById("contents");
-            if (contents) {
-                contents.innerHTML = "Contents";
-            }
-        }
         entries.forEach(entry => {
-            if (activeElement) {
-                document.querySelectorAll("nav[id='TableOfContents'] li").forEach((node) => {
-                    node.classList.add('inactive');
-                    node.classList.replace('active', 'inactive');
-                });
-            }
-            if (entry.intersectionRatio > 0) {
-                activeElement = entry.target.getAttribute('id');
-            }
-            if (activeElement) {
-                document.querySelector(`nav[id='TableOfContents'] li a[href="#${activeElement}"]`).parentElement.classList.replace('inactive', 'active');
+            const id = entry.target.firstElementChild.getAttribute("id");
+            if (entry.isIntersecting) {
+                document.querySelector(`#TableOfContents ul li a[href="#${id}"]`).parentElement.classList.add("active");
+            } else {
+                document.querySelector(`#TableOfContents ul li a[href="#${id}"]`).parentElement.classList.remove("active");
             }
         });
     });
 
-    const post = document.querySelector(".post");
-    post.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]").forEach((section) => {
+    document.querySelectorAll("section").forEach((section) => {
         observer.observe(section);
     });
 });
